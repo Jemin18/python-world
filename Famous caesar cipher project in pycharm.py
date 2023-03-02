c1655acb0115dec0076ce07097822ec1ad71654d
Jemin18 <125953990+Jemin18@users.noreply.github.com>
2023-03-02 22:59:55 +0530

    Add files via upload
    In this command line python project, you can encode and decode your message by certain amount of shift number.

---
+++ Famous caesar cipher project in pycharm.py
@@ -0,0 +1,74 @@
+logo = """
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88
+            88             88
+           ""             88
+                          88
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
+8b         88 88       d8 88       88 8PP""""""" 88
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88
+              88
+              88
+"""
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z',]
+
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    if direction.lower() == "encode":
+        def encrypt(a, b):
+            message = ""
+            for char in text:
+                if char in alphabet:
+                    index_in_list = alphabet.index(char)
+                    index_in_list += shift
+                    new_list = alphabet[index_in_list]
+                    n = new_list
+                    message += n
+                else:
+                    message += char
+            print(f"Your encoded message is {message}.")
+
+        encrypt(a=text, b=shift)
+
+    elif direction.lower() == "decode":
+        def decrypt(a,b):
+            message = ""
+            for char in text:
+                if char in alphabet:
+                    index_in_list = alphabet.index(char)
+                    index_in_list -= shift
+                    new_list = alphabet[index_in_list]
+                    n = new_list
+                    message += n
+                else:
+                    message += char
+            print(f"Your decoded message is {message}.")
+
+        decrypt(a=text, b=shift)
+
+    msg = input("Type 'yes' if you want to go again, otherwise type 'no':\n")
+    if msg.lower() == "no":
+        break
+
+
+
+
+
+
+
+
+
