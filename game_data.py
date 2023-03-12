692f6dca2c3ef8f97722067772dac38ab69da6a2
Jemin18 <125953990+Jemin18@users.noreply.github.com>
2023-03-12 19:23:30 +0530

    Add files via upload
    Data file for higher-lower instagram followers game.

---
+++ game_data.py
@@ -0,0 +1,249 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 613,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 556,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 359,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 367,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 395,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 380,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 347,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 437,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 206,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 264,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 280,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Ranveer Sngh',
+        'follower_count': 43.2,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Katrina Kaif',
+        'follower_count': 71,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Kapil Sharma',
+        'follower_count': 44.1,
+        'description': 'Comedian',
+        'country': 'India'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 248,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Hrithik Roshan',
+        'follower_count': 45,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Salman Khan',
+        'follower_count': 58.9,
+        'description': 'Actor and philanthropist',
+        'country': 'India'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 236,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jacqueline Fernandez',
+        'follower_count': 65.6,
+        'description': 'Actress',
+        'country': 'Sri Lanka'
+    },
+    {
+        'name': 'Indian cricket team',
+        'follower_count': 27.8,
+        'description': 'Bcci cricket team',
+        'country': 'India'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 275,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 167,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 134,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 118,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Alia Bhatt',
+        'follower_count': 75.7,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 84.2,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 147,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Vin Diesel',
+        'follower_count': 89.6,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Akshay Kumar',
+        'follower_count': 64.5,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'David Beckham',
+        'follower_count': 77.8,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'UEFA Champions League',
+        'follower_count': 103,
+        'description': 'Club football competition',
+        'country': 'Europe'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 89.1,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Deepika Paducone',
+        'follower_count': 72.5,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 71.5,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 73.2,
+        'description': 'Prime Minister',
+        'country': 'India'
+    },
+    {
+        'name': 'Neha Kakkar',
+        'follower_count': 73.9,
+        'description': 'Singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 239,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 85.5,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 72,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 77,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shraddha kapoor',
+        'follower_count': 78.9,
+        'description': 'Actress',
+        'country': 'India'
+    },
+
+]
